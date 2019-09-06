f3320d783cb2f250b5147effa9105b14c3e5b596
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-06 15:53:11 +0300

    welcome page screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Payment Getway
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="">Payment Check</a>
+                    <a href="">Providers List</a>
+                    <a href="">Docs</a>
+                    <a href="">API</a>
+                    <a href="">Contact Us</a>
                 </div>
             </div>
         </div>
