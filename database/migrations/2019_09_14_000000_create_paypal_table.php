d50a3dee8d7ef5a9aaa5b0ac39046dd05cbde35f
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-14 18:14:41 +0300

    option to disable the getway

--- database/migrations/2019_09_14_000000_create_paypal_table.php
+++ database/migrations/2019_09_14_000000_create_paypal_table.php
@@ -23,6 +23,7 @@ class CreatepaypalTable extends Migration
             $table->string('certificate')->default(0);
             $table->string('app_id')->default(0);
             $table->string('sandbox', 1)->default(0);
+            $table->string('disabled', 1)->default(0);
             $table->timestamps();
         });
     }
