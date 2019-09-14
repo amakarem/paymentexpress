28763bd6f664ffb36d3cebd8c8b00e4f09d4271d
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-14 16:04:25 +0300

    modal for setup paypal

--- resources/views/layouts/modals.blade.php
+++ resources/views/layouts/modals.blade.php
@@ -5,13 +5,21 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Paypal Setting</h4>
       </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
+      <form action="/action_page.php">
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="email">Paypal Account Email:</label>
+            <input type="hidden" id="getway" name="getway" value="paypal">
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+          </div>
+          <div class="checkbox">
+            <label><input type="checkbox" name="remember"> Remember me</label>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-success">Save</button>
+        </div>
+      </form>
     </div>
-
   </div>
 </div>
