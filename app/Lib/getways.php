a572ec933afbded5a67f661b4a74254d6744b238
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-14 15:15:39 +0300

    modal for setup paypal

---
+++ app/Lib/getways.php
@@ -0,0 +1,2 @@
+<?php
+$getwaylist = array('paypal');
--- resources/views/getways.blade.php
+++ resources/views/getways.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Payment Getways</div>
 
@@ -18,7 +18,7 @@
                     </thead>
                     <tbody>
                     <?php
-                    $getwaylist = array('paypal');
+                    require app_path() . '/Lib/getways.php';
                     foreach ($getwaylist as $getway) {
                         $getwaystatus = DB::table($getway)->where('owner', Auth::user()->id)->get()->toArray();
                         if(!empty($getwaystatus)) {
@@ -27,7 +27,7 @@
                             $action .= '<a href="" class="btn btn-sm btn-danger">Delete</a>';
                         } else {
                             $getwaystatus = 'Not set';
-                            $action = '<a href="" class="btn btn-sm btn-success">Setup</a>';
+                            $action = '<a href="" data-toggle="modal" data-target="#paypal" class="btn btn-sm btn-success">Setup</a>';
                         }
                         $getway = ucfirst($getway);
                         echo "<tr><td>$getway</td><td>$getwaystatus</td><td>$action</td></tr>";
@@ -40,4 +40,5 @@
         </div>
     </div>
 </div>
+@include('layouts.modals')
 @endsection
