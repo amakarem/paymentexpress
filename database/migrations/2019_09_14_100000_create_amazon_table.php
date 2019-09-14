205973842d0122d7470df9cf627c2c8493d9a9ec
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-14 18:49:50 +0300

    amazon table

---
+++ database/migrations/2019_09_14_100000_create_amazon_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateamazonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('amazon', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('owner');
+            $table->string('merchantId')->unique();
+            $table->string('accessKey')->default(0);
+            $table->string('secretKey')->default(0);
+            $table->string('clientid')->default(0);
+            $table->string('sandbox', 1)->default(0);
+            $table->string('disabled', 1)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('amazon');
+    }
+}
