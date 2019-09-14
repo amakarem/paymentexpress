06a5fd52f791e8c2ad350ee126e3ca73682caf7f
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-14 12:55:18 +0300

    new ver

--- database/migrations/2019_09_01_000000_create_payment_table.php
+++ database/migrations/2019_09_01_000000_create_payment_table.php
@@ -15,12 +15,14 @@ class CreatepaymentTable extends Migration
     {
         Schema::create('payment', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('provider');
+            $table->integer('owner');
+            $table->string('orderid')->unique();
             $table->string('getway');
             $table->string('txnid');
-            $table->string('payment_amount');
-            $table->string('payment_status');
+            $table->string('amount');
+            $table->string('status');
             $table->string('details');
+            $table->string('callback');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2019_09_14_000000_create_paypal_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatepaypalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paypal', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('owner');
+            $table->string('email')->unique();
+            $table->string('username')->default(0);
+            $table->string('password')->default(0);
+            $table->string('secret')->default(0);
+            $table->string('certificate')->default(0);
+            $table->string('app_id')->default(0);
+            $table->string('sanbox', 1)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paypal');
+    }
+}
