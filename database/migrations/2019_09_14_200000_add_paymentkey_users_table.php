505c34d7f19d19398381461a20175cc9b04d5dc6
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-14 19:41:44 +0300

    save payment key to users table

---
+++ database/migrations/2019_09_14_200000_add_paymentkey_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPaymentkeyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('paymentkey')->default(0);
+        });
+        DB::statement("ALTER TABLE users MODIFY paymentkey varchar(30) AFTER email");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('paymentkey');
+        });
+    }
+}
