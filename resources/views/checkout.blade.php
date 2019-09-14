8a3a6ae166456015302a8981c29b0321e1be54f8
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-09-14 13:31:52 +0300

    optmize errors seprate getways tables

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -12,31 +12,36 @@ if (!empty($_POST)) {
 if (isset($order['account']) && isset($order['id']) && isset($order['details']) && isset($order['amount'])) {
     $paymentdata = array();
     $account = base64_decode($order['account']);
-    $users = DB::table('users')->where('email', $account)->get();
-    foreach ($users as $user) {
-        $accountname = $user->name;
-        //echo base64_encode($user->email);
-        echo "<br>Order Number: " . $order['id'];
-        echo "<br>Seller: " . $accountname;
-        echo "<br>Order Details: " . $order['details'];
-        echo "<br><b>Total</b>: $" . $order['amount'];
-        echo "<hr><br><b>Available Paymet method:</b><br>";
-        if ($user->paypal != '') {
-            $paypalmethod = array(
-                'business' => $user->paypal,
-                'cmd' => '_xclick',
-                'notify_url' => '',
-                'item_name' => $order['details'],
-                'bn' => $order['id'],
-                'amount' => $order['amount'],
-            );
-            echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">';
-            foreach ($paypalmethod as $key => $value) {
-                echo '<input type="hidden" name="' . $key . '" value="' . $value . '">';
+    $users = DB::table('users')->where('email', $account)->get()->toArray();
+    if(!empty($users)) {
+        foreach ($users as $user) {
+            $accountname = $user->name;
+            //echo base64_encode($user->email);
+            echo "<br>Order Number: " . $order['id'];
+            echo "<br>Seller: " . $accountname;
+            echo "<br>Order Details: " . $order['details'];
+            echo "<br><b>Total</b>: $" . $order['amount'];
+            echo "<hr><br><b>Available Paymet method:</b><br>";
+            $paypal = DB::table('paypal')->where('owner', $user->id)->get()->toArray();
+            if (!empty($paypal)) {
+                $paypalmethod = array(
+                    'business' => $paypal->email,
+                    'cmd' => '_xclick',
+                    'notify_url' => '',
+                    'item_name' => $order['details'],
+                    'bn' => $order['id'],
+                    'amount' => $order['amount'],
+                );
+                echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">';
+                foreach ($paypalmethod as $key => $value) {
+                    echo '<input type="hidden" name="' . $key . '" value="' . $value . '">';
+                }
+                echo '<input type="image" name="submit" border="0" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_150x38.png" alt="PayPal - The safer, easier way to pay online">';
+                echo '</form>';
             }
-            echo '<input type="image" name="submit" border="0" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_150x38.png" alt="PayPal - The safer, easier way to pay online">';
-            echo '</form>';
         }
+    } else {
+        echo "Account not found";
     }
 } else {
     echo "WRONG REQUEST";
