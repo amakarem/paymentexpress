e6d1f6e39505fd2deff0c3a823ca90466ba1d79e
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-10-16 17:02:40 +0300

    show simple code for integration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,14 +18,25 @@
                     <br>
                     <pre>
                     <?php
-                        $paymentkey = Auth::user()->paymentkey;
-                        if ($paymentkey == '' || $paymentkey == '0') {
-                            echo "<br><b>You don't have Payment Key </b>";
-                            echo '<a class="btn btn-success" href="' . route('generatepaymentkey') . '">Generate Payment Key</a>';
-                        } else {
-                            echo "<b>Your payment Key is: </b><kbd>$paymentkey</kbd>";
-                        }
-                    ?>
+$paymentkey = Auth::user()->paymentkey;
+if ($paymentkey == '' || $paymentkey == '0') {
+    echo "<br><b>You don't have Payment Key </b>";
+    echo '<a class="btn btn-success" href="' . route('generatepaymentkey') . '">Generate Payment Key</a>';
+} else {
+    echo "<b>Your payment Key is: </b><kbd>$paymentkey</kbd>";
+    echo "<hr><b>Here is your simple HTML code for integration</b><br>";
+    echo '
+<textarea rows="10" cols="60" name="code">
+<form method="GET" action="https://' . $_SERVER['REQUEST_URI'] . '/checkout">
+    <input type="hidden" name="account" value="' . $paymentkey . '">
+    <input type="hidden" name="id" value="2">
+    <input type="text" name="details" value="details for the order">
+    <input type="text" name="amount" value="100">
+    <input type="submit" value="submit">
+</form>
+</textarea>';
+}
+?>
                 </div>
             </div>
         </div>
