4b8ce2a53588b13f5e243a28ff1b065666638e91
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-11-13 20:26:46 +0200

    list paypal transaction

--- resources/views/paymentlist.blade.php
+++ resources/views/paymentlist.blade.php
@@ -1,3 +1,79 @@
+<?php
+
+$data = DB::table('paypal')->where('owner', Auth::user()->id)->get();
+$data = json_decode(json_encode($result), true);
+$client_id = $data['paypal']['username'];
+$secret = $data['paypal']['password'];
+function token($client_id, $secret)
+{
+    global $system;
+    $queryUrl = $system . 'oauth2/token';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $queryUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+    curl_setopt($ch, CURLOPT_USERPWD, $client_id . ':' . $secret);
+    $headers = array();
+    $headers[] = 'Accept: application/json';
+    $headers[] = 'Accept-Language: en_US';
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    $result = json_decode($result, true);
+    curl_close($ch);
+    if (isset($result['access_token'])) {
+        $result = $result['access_token'];
+    }
+    return $result;
+}
+
+function gettransactions($token)
+{
+    global $system;
+    $queryUrl = $system . 'reporting/transactions?start_date=2019-09-01T00:00:00-0700&end_date=2019-09-30T23:59:59-0700&fields=transaction_info,payer_info&page_size=500&page=1';
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $queryUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+    $headers = array();
+    $headers[] = 'Content-Type: application/json';
+    $headers[] = 'Authorization: Bearer ' . $token;
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close($ch);
+    $result = json_decode($result, true);
+    $result = $result['transaction_details'];
+    return $result;
+}
+
+$token = token($client_id, $secret);
+
+$result = gettransactions($token);
+$transactions = array();
+foreach ($result as $value) {
+    if (!isset($value['transaction_info']['paypal_reference_id'])) {
+        $payer = $value['payer_info']['email_address'];
+        $value = $value['transaction_info'];
+        $transactions[$value['transaction_id']]['ID'] = $value['transaction_id'];
+        $transactions[$value['transaction_id']]['Date'] = $value['transaction_updated_date'];
+        $transactions[$value['transaction_id']]['Currency'] = $value['transaction_amount']['currency_code'];
+        $transactions[$value['transaction_id']]['value'] = $value['transaction_amount']['value'];
+        $transactions[$value['transaction_id']]['payer'] = $payer;
+    }
+}
+print_r($transactions);
+?>
+
 @extends('layouts.app')
 
 @section('content')
