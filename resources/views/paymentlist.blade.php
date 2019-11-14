ce93d8d625413bd85680a24aadcaaaa817fcc0f3
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-11-14 16:39:44 +0200

    show date range error

--- resources/views/paymentlist.blade.php
+++ resources/views/paymentlist.blade.php
@@ -1,34 +1,4 @@
 <?php
-
-$data = DB::table('paypal')->where('owner', Auth::user()->id)->get();
-$data = json_decode(json_encode($data), true);
-
-$client_id = $data[0]['username'];
-$secret = $data[0]['password'];
-$sandbox = $data[0]['sandbox'];
-global $system;
-$system = 'https://api.paypal.com/v1/';
-if ($sandbox == 1) {
-    $system = 'https://api.sandbox.paypal.com/v1/';
-}
-$start = date("Y-m-d",strtotime("-1 month"));
-$end = date("Y-m-d");
-if(isset($_GET['start'])) {
-    $start = $_GET['start'];
-}
-if(isset($_GET['end'])) {
-    $end = $_GET['end'];
-}
-$start = $start . 'T00:00:00-0000';
-$end = $end . 'T23:59:59-0000';
-
-$date1 = date_create($start);
-$date2 = date_create($end);
-$diff = date_diff($date1, $date2);
-$age = $diff->format("%a");
-echo $age;
-
-
 function token($client_id, $secret)
 {
     global $system;
@@ -86,22 +56,51 @@ function gettransactions($token,$start,$end)
     return $result;
 }
 
-$token = token($client_id, $secret);
+$data = DB::table('paypal')->where('owner', Auth::user()->id)->get();
+$data = json_decode(json_encode($data), true);
 
-$result = gettransactions($token,$start,$end);
-$transactions = array();
-foreach ($result as $value) {
-    if (!isset($value['transaction_info']['paypal_reference_id']) && isset($value['payer_info']['email_address'])) {
-        $payer = $value['payer_info']['email_address'];
-        $value = $value['transaction_info'];
-        $transactions[$value['transaction_id']]['id'] = $value['transaction_id'];
-        $transactions[$value['transaction_id']]['date'] = $value['transaction_updated_date'];
-        $transactions[$value['transaction_id']]['currency'] = $value['transaction_amount']['currency_code'];
-        $transactions[$value['transaction_id']]['value'] = $value['transaction_amount']['value'];
-        $transactions[$value['transaction_id']]['payer'] = $payer;
-    }
+$client_id = $data[0]['username'];
+$secret = $data[0]['password'];
+$sandbox = $data[0]['sandbox'];
+global $system;
+$system = 'https://api.paypal.com/v1/';
+if ($sandbox == 1) {
+    $system = 'https://api.sandbox.paypal.com/v1/';
 }
+$start = date("Y-m-d",strtotime("-1 month"));
+$end = date("Y-m-d");
+if(isset($_GET['start'])) {
+    $start = $_GET['start'];
+}
+if(isset($_GET['end'])) {
+    $end = $_GET['end'];
+}
+$start = $start . 'T00:00:00-0000';
+$end = $end . 'T23:59:59-0000';
 
+$date1 = date_create($start);
+$date2 = date_create($end);
+$diff = date_diff($date1, $date2);
+$age = $diff->format("%a");
+
+$transactions = array();
+if($age <= 31) {
+    $token = token($client_id, $secret);
+    $result = gettransactions($token,$start,$end);
+    foreach ($result as $value) {
+        if (!isset($value['transaction_info']['paypal_reference_id']) && isset($value['payer_info']['email_address'])) {
+            $payer = $value['payer_info']['email_address'];
+            $value = $value['transaction_info'];
+            $transactions[$value['transaction_id']]['id'] = $value['transaction_id'];
+            $transactions[$value['transaction_id']]['date'] = $value['transaction_updated_date'];
+            $transactions[$value['transaction_id']]['currency'] = $value['transaction_amount']['currency_code'];
+            $transactions[$value['transaction_id']]['value'] = $value['transaction_amount']['value'];
+            $transactions[$value['transaction_id']]['payer'] = $payer;
+        }
+    }
+} else {
+    $error = 'Date range is greater than 31 days';
+}
 ?>
 
 @extends('layouts.app')
